fcb4c1c2e750869689ad0193c0db9090c4407ca9
Dr. Stefan Gruenwald <sgruenwald@cgfunding.com>
2016-05-01 13:22:11 -0700

    Added files via upload

---
+++ e033.py
@@ -0,0 +1,24 @@
+from fractions import *
+from itertools import *
+
+def is_curious(n, d):
+    f = Fraction(n, d)
+    if f >= 1:
+        return False
+    n_digits = [int(digit) for digit in str(n)]
+    d_digits = [int(digit) for digit in str(d)]
+
+    if n_digits[1] == d_digits[0]:
+        try:
+            if Fraction(n_digits[0], d_digits[1]) == f:
+                return True
+        except:
+            pass
+    return False
+    
+def main():
+    fractions = product(range(10, 100), range(10, 100))
+    print reduce(lambda a, b: a * b, (Fraction(*f) for f in fractions if is_curious(*f))).denominator
+
+if __name__ == "__main__":
+    main()
