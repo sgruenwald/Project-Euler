b9086f83d369074084bad5f44e649ded049d0f66
Dr. Stefan Gruenwald <sgruenwald@cgfunding.com>
2016-05-01 13:29:12 -0700

    Added files via upload

---
+++ e052.py
@@ -0,0 +1,21 @@
+def get_digits(n):
+    return sorted(str(n))
+
+def same_digits(n, iterations=2):
+    digits = get_digits(n)
+    for i in range(1, iterations):
+        if not digits == get_digits(n+(i*n)):
+            return False
+    return True
+
+def main():
+    n = 0
+    found = False
+    while not found:
+        n += 1
+        if same_digits(n,6):
+            found = True
+    print n
+
+if __name__ == "__main__":
+    main()
