ca3a2ead1fd5701a83303b4a0af8169d8b508ef0
Dr. Stefan Gruenwald <sgruenwald@cgfunding.com>
2016-05-02 12:31:32 -0700

    Update e021.py

--- e021.py
+++ e021.py
@@ -3,17 +3,16 @@ def amicableNumber():
     def sumOfDivisors(numb):
         sumD = 0
         for d in range(numb/2):
-            d = d+1
+            d += 1
             if numb%d ==0:
-                sumD = sumD + d
+                sumD += d
         return sumD
     for i in range(10000-1):
-        i = i+1
+        i += 1
         if i == sumOfDivisors(i):
             continue
         if i == sumOfDivisors(sumOfDivisors(i)):
-            print i
-            totalSum = totalSum + i    
-    print "Evaluate the sum of all the amicable numbers under 10000:",totalSum
+            totalSum += i    
+    print totalSum
     
 amicableNumber()
