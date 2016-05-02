e1ea2b957b34d0388724c80814eb09d8c621313d
Dr. Stefan Gruenwald <sgruenwald@cgfunding.com>
2016-05-02 12:56:34 -0700

    Update e042.py

--- e042.py
+++ e042.py
@@ -1,23 +1,15 @@
-import os
-from string import ascii_uppercase
+import string
 
-def letter_value(letter):
-    return ascii_uppercase.index(letter.upper())+1
+def score(word) :
+	
+	return sum((1 + string.uppercase.index(c)) for c in word)
 
-def word_value(word):
-    return sum(letter_value(letter) for letter in word)
+triangles = [ (n*(n+1))/2 for n in range(100) ]
+words = {}
+f = open ("words-42.txt")
+for line in f.readlines() :
+	for w in line.replace('"', "").strip().split(',') :
+		words[w] = score(w)
 
-def triangle(n):
-    return int((0.5) * n * (n+1))
-
-def main():
-    triangle_numbers = [triangle(n) for n in range(1, 100)]
-    words = [word.strip('"') for word in open(os.path.join(os.path.dirname(__file__), 'words.txt')).read().split(',')]
-    total = 0
-    for word in words:
-        if word_value(word) in triangle_numbers:
-            total += 1
-    print total
-
-if __name__ == "__main__":
-    main()
+result = sum(1 for k, v in words.items() if v in triangles)
+print result
