f4e77b268685546d4fd3282fa09f390ce9f749aa
Dr. Stefan Gruenwald <sgruenwald@cgfunding.com>
2016-05-02 13:41:36 -0700

    Update e074.py

--- e074.py
+++ e074.py
@@ -3,6 +3,8 @@ from math import factorial
 d = { 169:3, 363601:3, 1454:3, 871:2, 45361:2, 872:2, 45362:2 }
 def euler74(n) :
 	global d
+	#print "euler74(%d)" % n
+	
 	if d.has_key(n) :
 		return d[n]
 	else :
@@ -13,12 +15,10 @@ def euler74(n) :
 		d[n] = result
 		return result
 
-def test():
+def testAll ():
 	for i in xrange(1, 1000*1000) :
-		print i, "\r",
 		euler74(i)
-	print
 	print sum((1 if v == 60 else 0) for v in d.itervalues())
 
 if __name__ == "__main__" :
-	test()
+	testAll()
